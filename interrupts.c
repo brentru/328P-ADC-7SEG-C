545f5e4694e678a26f89a08089b81eca42e45e39
Brent R <codingbrent@users.noreply.github.com>
2016-03-29 16:41:15 -0400

    from lab #2

--- interrupts.c
+++ interrupts.c
@@ -4,15 +4,9 @@
 // libs
 #include <avr/interrupt.h>
 #include <avr/io.h>
-#include <util/delay.h>
-
-int main(void)
-{
-
-	// 7-seg code inits
-	int printnum = 4; // qty of #'s on 7seg
-	const uint_8 cathodelut[4] PROGMEM = { 0x01, 0x02, 0x08, 0x04};
-	const uint_8 binlut[10] PROGMEM ={
+#include <avr/pgmspace.h>
+	const uint8_t cathodelut[4] PROGMEM = { 0x01, 0x02, 0x08, 0x04};
+	const uint8_t binlut[15] PROGMEM ={
 		0b00111111, /* 0 */
 		0b00000110, /* 1 */
 		0b01010011, /* 2 */
@@ -31,19 +25,38 @@ int main(void)
 		0b01110001  /* E */
 
 	};
+volatile uint8_t segupdate = 0;
+volatile uint8_t adcupdate = 0;
+	
+int main(void)
+{
+	 uint16_t myVal = ADC;
+	// 7-seg code inits
+	int printnum = 4; // qty of #'s on 7seg
 
 
+	uint8_t currentDigit; // counts from 0=>3, tells which cathode you're supposed to drive. grab individual decimal digits of myval based off of this. 
+	// setup timer @ 240 hz
+	// ctc mode
+	// clk div / 1024
+	// timer compare Init.
+	// OCR0A
+	TCCR0A |= (1<<WGM01) | (1<<CS02) | (1<<CS00);
+	TCNT0 = 0;
+	
 	// interrupt setup
 	ADCSRA |= (1<<ADEN); // ADC enabled pg 249, bottom of page
 	// 50kHz < clk freq < 200kHz (pg240)
-	ADCSRA |= (7<<ADPS0;) // prescaler w/128 division factor
-	ADMUX |= (0b01<<REFS0); // uses the AVCC voltage 
-	ADCSRA |= (1<<ADIE) // bit write to 1, when sreg's "i" is set, the ADC conversion complete interrupt is enabled (pg250)
+	ADCSRA |= (1<<ADPS0) | (1<<ADPS1) | (1<<ADPS2); // prescaler w/128 division factor
+	ADMUX |= (1<<REFS0); // uses the AVCC voltage
+	ADCSRA |= (1<<ADIE); // bit write to 1, when sreg's "i" is set, the ADC conversion complete interrupt is enabled (pg250)
 
 
 	// portb setup
-	DDRB0 &= ~(1<<DDB0); // clear the PB0
-	// PB0 is now an input 
+	// pb7 => pcint7
+	// pcint7 => pcint0
+	DDRB &= ~(1<<DDB0); // clear the PB0
+	// PB0 is now an input
 	PORTB |= (1<<PORTB0); // turn on pullup
 	//pb0 is now an input w/pull-up enabled
 	PCICR |= (1<<PCIE0); // enable PCMSK0 scan
@@ -54,24 +67,23 @@ int main(void)
 
 	while(1)
 	{
-		PINC = STUFF NEEDS TO GO HERE
+		PINC ^= _BV(1); // toggle the pinc
 		// do nothing b/c interrupt is handling this instead
-	}	
+		if (segupdate == 1)
+		{
+			//update 7seg
+		}
+		else if(adcupdate == 1)
+		{
+			//update ADC
+		}
+	}
 }
 
-uint16_t GetADC(uint8_t ADCh)
-{
-	// masking & channel select (ADCh is channel)
-	ADMUX = (ADMUX & 0b00001111) | (ADCh & 0b00001111); 
-	ADCSRA |= (1<<ADSC); 
 
-	// wait for adc conversion to finish
-	while((ADCSRA & _BV(ADSC)));
-	// return ADC result
-	return ADC;
-}
 
-int printLUT(void)
+
+void printLUT(void)
 {
 	DDRB = cathodelut[0];
 	PORTB = cathodelut[0];
@@ -79,13 +91,13 @@ int printLUT(void)
 	PORTD = binlut[0];
 }
 
-ISR (ADC_VECT)
+ISR (ADC_vect)
 {
 	// do stuff with adc here
 }
-ISR (PCINT0_VECT)
+ISR (PCINT0_vect)
 {
-	// 
+	//
 	if((PINB &(1<<PINB0)) == 1)
 	{
 		// low to high pin change
